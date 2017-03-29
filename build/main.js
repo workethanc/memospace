baf333393e6b5c89c0605930300bd1c3f703fa2e
workethanc <ciethanc@gmail.com>
2017-03-29 21:50:25 +0900

    set npm

--- build/main.js
+++ build/main.js
@@ -8,74 +8,24 @@ var _path = require('path');
 
 var _path2 = _interopRequireDefault(_path);
 
-var _webpack = require('webpack');
-
-var _webpack2 = _interopRequireDefault(_webpack);
-
 var _webpackDevServer = require('webpack-dev-server');
 
 var _webpackDevServer2 = _interopRequireDefault(_webpackDevServer);
 
-var _morgan = require('morgan');
+var _webpack = require('webpack');
 
-var _morgan2 = _interopRequireDefault(_morgan);
-
-var _bodyParser = require('body-parser');
-
-var _bodyParser2 = _interopRequireDefault(_bodyParser);
-
-var _mongoose = require('mongoose');
-
-var _mongoose2 = _interopRequireDefault(_mongoose);
-
-var _expressSession = require('express-session');
-
-var _expressSession2 = _interopRequireDefault(_expressSession);
-
-var _routes = require('./routes');
-
-var _routes2 = _interopRequireDefault(_routes);
+var _webpack2 = _interopRequireDefault(_webpack);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// HTTP REQUEST LOGGER
-var app = (0, _express2.default)(); // PARSE HTML BODY
-
-var port = 3000;
 var devPort = 4000;
-
-app.use((0, _morgan2.default)('dev'));
-app.use(_bodyParser2.default.json());
-
-/* mongodb connection */
-var db = _mongoose2.default.connection;
-db.on('error', console.error);
-db.once('open', function () {
-    console.log('Connected to mongodb server');
-});
-// mongoose.connect('mongodb://username:password@host:port/database=');
-_mongoose2.default.connect('mongodb://localhost/codelab');
-
-/* use session */
-app.use((0, _expressSession2.default)({
-    secret: 'CodeLab1$1$234',
-    resave: false,
-    saveUninitialized: true
-}));
+var app = (0, _express2.default)();
+var port = 3000;
 
 app.use('/', _express2.default.static(_path2.default.join(__dirname, './../public')));
 
-/* setup routers & static directory */
-app.use('/api', _routes2.default);
-
-app.get('*', function (req, res) {
-    res.sendFile(_path2.default.resolve(__dirname, './../public/index.html'));
-});
-
-/* handle error */
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+app.get('/hello', function (req, res) {
+    return res.send('Hello CodeLab');
 });
 
 app.listen(port, function () {
